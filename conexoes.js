4fb1dc36946d0548efe2d574617fbbc82ec9a621
Hugo Hoffmann <hugoalvesdesolza@gmail.com>
2020-11-13 09:00:39 -0300

    😜  agora vai para a proxima pagina automaticamente

--- conexoes.js
+++ conexoes.js
@@ -1,5 +1,10 @@
 function startBotConexoes() {
   let contador = 0;
+  let conexao = 0;
+
+  document.querySelectorAll('.search-result__action-button').forEach((item, idx) => {
+    contador++;
+  });
 
   document.querySelectorAll('.search-result__action-button').forEach((item, idx) => {
     setTimeout(() => {
@@ -9,25 +14,26 @@ function startBotConexoes() {
           var modalFooter = document.querySelector(".artdeco-modal__actionbar")
           modalFooter.children[1].click()
 
-          contador++;
-          console.log(`Seu BOT já conectou ${contador} pessoa(s)!`);
+          conexao++;
+          contador--;
+          console.log(`Seu BOT já conectou ${conexao} pessoa(s)!`);
       } else { 
+        contador--;
         console.log('Você já segue essa pessoa.');
       }
-    }, idx * 7000);
 
-    /*
-    if(!document.querySelectorAll('.search-result__action-button')[idx+1]) {
-      document.querySelectorAll('.artdeco-pagination__indicator').forEach((item, idx) => {
-        if(item.classList.contains("selected")) {
-          var nextPag = document.querySelectorAll('.artdeco-pagination__indicator')[idx+1]
-          nextPag.children[0].click()
-        }
-      })
-
-      this.startBotConexoes();
-    }
-    */
+      if(contador <= 0) {
+        document.querySelectorAll('.artdeco-pagination__indicator').forEach((item, idx) => {
+          if(item.classList.contains("selected")) {
+            var nextPag = document.querySelectorAll('.artdeco-pagination__indicator')[idx+1]
+              nextPag.children[0].click()
+            setTimeout(() => {
+              this.startBotConexoes()
+            }, 6000)
+          }
+        })
+      }
+    }, idx * 6000);
   });
 }
 
