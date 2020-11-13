25db1b04fb2371ed52ffa8461fe0eaee4dd3f5c8
Hugo Hoffmann <hugoalvesdesolza@gmail.com>
2020-11-13 17:09:47 -0300

    🧐  foi dado um tapinha no codigo; e agora joga para o fim da pagina para carregar todos os elementos

--- conexoes.js
+++ conexoes.js
@@ -2,39 +2,49 @@ function startBotConexoes() {
   let contador = 0;
   let conexao = 0;
 
-  document.querySelectorAll('.search-result__action-button').forEach((item, idx) => {
-    contador++;
-  });
+  scrollTo(0, document.querySelector('body').scrollHeight);
 
-  document.querySelectorAll('.search-result__action-button').forEach((item, idx) => {
-    setTimeout(() => {
-      if(!item.disabled){
-          item.click();
-          
-          var modalFooter = document.querySelector(".artdeco-modal__actionbar")
-          modalFooter.children[1].click()
+  const nextpage = () => {
+    if (contador <= 0) {
+      document.querySelectorAll('.artdeco-pagination__indicator').forEach((item, idx) => {
+        if (item.classList.contains("selected")) {
+          var nextPag = document.querySelectorAll('.artdeco-pagination__indicator')[idx + 1]
+          nextPag.children[0].click()
+          setTimeout(() => {
+            this.startBotConexoes()
+          }, 6000)
+        }
+      })
+    }
+  }
 
-          conexao++;
-          contador--;
-          console.log(`Seu BOT já conectou ${conexao} pessoa(s)!`);
-      } else { 
+  setTimeout(() => {
+    document.querySelectorAll('.search-result__action-button').forEach((item, idx) => {
+      contador++;
+    });
+
+    document.querySelectorAll('.search-result__action-button').forEach((item, idx) => {
+      if (item.innerHTML.trim().toLowerCase() != "conectar") {
         contador--;
-        console.log('Você já segue essa pessoa.');
+        console.log(`${contador} pessoa que talvez você seque!`);
+        nextpage()
+        return;
       }
 
-      if(contador <= 0) {
-        document.querySelectorAll('.artdeco-pagination__indicator').forEach((item, idx) => {
-          if(item.classList.contains("selected")) {
-            var nextPag = document.querySelectorAll('.artdeco-pagination__indicator')[idx+1]
-              nextPag.children[0].click()
-            setTimeout(() => {
-              this.startBotConexoes()
-            }, 6000)
-          }
-        })
-      }
-    }, idx * 6000);
-  });
+      setTimeout(() => {
+        item.click();
+
+        var modalFooter = document.querySelector(".artdeco-modal__actionbar")
+        modalFooter.children[1].click()
+
+        conexao++;
+        contador--;
+        console.log(`Seu BOT já conectou ${conexao} pessoa(s)!`);
+
+        nextpage()
+      }, idx * 6000);
+    });
+  }, 7000)
 }
 
 this.startBotConexoes()
