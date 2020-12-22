f88921e5c5c8f567b77ea3c1c1290a9f4f7c9e2b
Hugo Hoffmann <hugoalvesdesolza@gmail.com>
2020-12-22 04:22:36 +0000

    add rand time for follow

--- conexoes.js
+++ conexoes.js
@@ -1,49 +1,53 @@
 function startBotConexoes() {
-  let contador = 0;
-  let conexao = 0;
+    let contador = 0;
+    let conexao = 0;
 
-  scrollTo(0, document.querySelector('body').scrollHeight);
+    scrollTo(0, document.querySelector('body').scrollHeight);
 
-  const nextpage = () => {
-    if (contador <= 0) {
-      document.querySelectorAll('.artdeco-pagination__indicator').forEach((item, idx) => {
-        if (item.classList.contains("selected")) {
-          var nextPag = document.querySelectorAll('.artdeco-pagination__indicator')[idx + 1]
-          nextPag.children[0].click()
-          setTimeout(() => {
-            this.startBotConexoes()
-          }, 6000)
+    const nextpage = () => {
+        if (contador <= 0) {
+            document.querySelectorAll('.artdeco-pagination__indicator').forEach((item, idx) => {
+                if (item.classList.contains("selected")) {
+                    var nextPag = document.querySelectorAll('.artdeco-pagination__indicator')[idx + 1]
+                    nextPag.children[0].click()
+                    setTimeout(() => {
+                        this.startBotConexoes()
+                    }, 6000)
+                }
+            })
         }
-      })
     }
-  }
 
-  setTimeout(() => {
-    document.querySelectorAll('button[class="artdeco-button artdeco-button--2 artdeco-button--secondary ember-view"]').forEach((item, idx) => {
-      contador++;
-    });
+    const getRndInteger = (min, max) => {
+        return Math.floor(Math.random() * (max - min)) + min;
+    }
 
-    document.querySelectorAll('button[class="artdeco-button artdeco-button--2 artdeco-button--secondary ember-view"]').forEach((item, idx) => {
-      if (item.innerText.trim().toLowerCase() != "conectar") {
-        contador--;
-        nextpage()
-        return;
-      }
+    setTimeout(() => {
+        document.querySelectorAll('button[class="artdeco-button artdeco-button--2 artdeco-button--secondary ember-view"]').forEach((item, idx) => {
+            contador++;
+        });
 
-      setTimeout(() => {
-        item.click();
+        document.querySelectorAll('button[class="artdeco-button artdeco-button--2 artdeco-button--secondary ember-view"]').forEach((item, idx) => {
+            if (item.innerText.trim().toLowerCase() != "conectar") {
+                contador--;
+                nextpage()
+                return;
+            }
 
-        var modalFooter = document.querySelector(".artdeco-modal__actionbar")
-        modalFooter.children[1].click()
+            setTimeout(() => {
+                item.click();
 
-        conexao++;
-        contador--;
-        console.log(`Seu BOT já conectou ${conexao} pessoa(s)!`);
+                var modalFooter = document.querySelector(".artdeco-modal__actionbar")
+                modalFooter.children[1].click()
 
-        nextpage()
-      }, idx * 6000);
-    });
-  }, 7000)
+                conexao++;
+                contador--;
+                console.log(`Seu BOT já conectou ${conexao} pessoa(s)!`);
+
+                nextpage()
+            }, getRndInteger(3000, 10000));
+        });
+    }, 7000)
 }
 
 this.startBotConexoes()
